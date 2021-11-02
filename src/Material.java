2b7698d037ea8d76ed3f5b2f92ea94472b325f99
Gabriel Simon <gabrielsimon775@gmail.com>
2021-11-01 22:14:16 -0300

    Implementada classe responsável pelo material de construção

---
+++ src/Material.java
@@ -0,0 +1,28 @@
+
+public class Material {
+	
+	Integer idMaterial;
+	String nomeProduto;
+	TipoProduto tipoProduto;
+	
+	public Integer getIdMaterial() {
+		return idMaterial;
+	}
+	public void setIdMaterial(Integer idMaterial) {
+		this.idMaterial = idMaterial;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public TipoProduto getTipoProduto() {
+		return tipoProduto;
+	}
+	public void setTipoProduto(TipoProduto tipoProduto) {
+		this.tipoProduto = tipoProduto;
+	}
+	
+	
+}
---
+++ src/TipoProduto.java
@@ -0,0 +1,6 @@
+
+public enum TipoProduto {
+	TIJOLO,
+	CIMENTO,
+	PREGO
+}
